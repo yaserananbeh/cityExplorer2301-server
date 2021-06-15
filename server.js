4fb8f87010b000ff5f87bf10bbc1d0ba67041284
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-06-15 17:00:50 +0300

    add the modeling data

--- server.js
+++ server.js
@@ -7,14 +7,29 @@ app.use(cors())
 const weatherData=require('./data/weather.json');
 require('dotenv').config();
 const PORT=process.env.PORT
+const weatherBitData=process.env.WEATHR_BIT_KEY;
+console.log(weatherBitData)
+
 app.get('/', 
  function (req, res) { 
   res.send('Hello World') 
 })
+
 app.get('/weather', 
 function (req, res) { 
-
- res.send(weatherData) 
-//  console.log(next)
+  const theNewData=weatherData.data.map(value=>{
+    let editedData=new Weather(value);
+    console.log(editedData);
+    return editedData;
+  });
+  res.send(theNewData);
+  
+ 
 })
+class Weather{
+  constructor(obj){
+    this.description=obj.weather.description
+    this.data=obj.valid_date
+  }
+}
 app.listen(PORT) 
