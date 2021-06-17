41c605d7e64d781235295789ae9c43612b2470b8
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-06-17 12:23:45 +0300

    finish the weather cache

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,23 +1,36 @@
 const axios = require('axios')
 require('dotenv').config();
 const weatherBitKey = process.env.WEATHER_API_KEY;
-const Weather=require('../models/weather.model');
+const Weather = require('../models/weather.model');
+const Cache = require('../helper/cashe');
+const cacheObj = new Cache()
+
 const weatherController = (req, res) => {
     const lat = req.query.lat;
     const lon = req.query.lon;
-    console.log(lat, lon);
     if (lat && lon) {
-      const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherBitKey}&lat=${lat}&lon=${lon}`;
-  
-      axios.get(weatherBitUrl).then(response => {
-          const responseData = response.data.data.map(obj => new Weather(obj));
-          res.json(responseData)
-      }).catch(error => {
-          res.send(error.message)
-      });
-  } else {
-      res.send('please provide the proper lat and lon')
-  }
-  }
+        const weatherKey = `weather-${lat}-${lon}`;
+        // console.log(`weather-${lat}-${lon}`);
+        if (cacheObj[weatherKey]) {
+            console.log('from cache');
+            res.json(cacheObj[weatherKey])
+        }
+        else {
 
-  module.exports=weatherController;
+            const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherBitKey}&lat=${lat}&lon=${lon}`;
+
+            axios.get(weatherBitUrl).then(response => {
+                const responseData = response.data.data.map(obj => new Weather(obj));
+                console.log('from the API');
+                res.json(responseData);
+                cacheObj[weatherKey] = responseData;
+            }).catch(error => {
+                res.send(error.message)
+            });
+        }
+    } else {
+        res.send('please provide the proper lat and lon')
+    }
+}
+
+module.exports = weatherController;
